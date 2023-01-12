c87ab65c981494dbea703377aaf62567ba44188a
Arundhati Mohanty <arundhati.mohanty1@gmail.com>
2023-01-12 23:23:26 +0530

    version 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String [] args) {
+        RWOnly object = new RWOnly();
+//        object.name() = "check"; //cant access as name is private
+//        System.out.println(object.name);
+        object.setName("check");
+        System.out.println(object.getName());
+    }
+
 }
